89d0656164466fc0875f0a8253d9065b75743227
satoshi <satoshikumagaimail@gmail.com>
2018-08-20 08:48:52 +0900

    【初回】

---
+++ app/src/main/java/com/satoshi/testfirebasemenu/ButtonData.java
@@ -0,0 +1,14 @@
+package com.satoshi.testfirebasemenu;
+
+public class ButtonData {
+    //共通：行、列、画像名、ボタンタイプ
+    //個別：URL、パッケージ名、電話番号
+    int row;
+    int column;
+    String imageName;
+    String buttonType;
+    String url;
+    String packageName;
+    String phoneNumber;
+
+}
