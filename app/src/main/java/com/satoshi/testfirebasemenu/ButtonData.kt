e98cd9218a89f84920fe7d8c8091f40052c19901
satoshi <satoshi-kumagai.cubesystem.co.jp>
2018-08-22 16:18:10 +0900

    Firebase上から画像データとボタン情報を取得し、画面に表示するよう修正

--- app/src/main/java/com/satoshi/testfirebasemenu/ButtonData.kt
+++ app/src/main/java/com/satoshi/testfirebasemenu/ButtonData.kt
@@ -1,17 +1,12 @@
 package com.satoshi.testfirebasemenu
 
 class ButtonData {
-    //共通：行、列、画像名、ボタンタイプ
-
-    var row: Int = 0
-    var column: Int = 0
+    //共通：画像名、ボタンタイプ
     var ImageName: String? = null
-    /*
-    internal var buttonType: String? = null
+    var type: String = "dummy"
 
     //個別：URL、パッケージ名、電話番号
-    internal var url: String? = null
-    internal var packageName: String? = null
-    internal var phoneNumber: String? = null
-*/
+    var url: String? = null
+    var packageName: String? = null
+    var phoneNumber: String? = null
 }
