7d6efe5a4392eb7eef1267659f5986b0a15e1135
satoshi <satoshi-kumagai.cubesystem.co.jp>
2018-08-22 16:18:42 +0900

    Firebase上から画像データとボタン情報を取得し、画面に表示するよう修正

--- app/src/main/java/com/satoshi/testfirebasemenu/MainActivity.kt
+++ app/src/main/java/com/satoshi/testfirebasemenu/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.satoshi.testfirebasemenu
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -14,16 +16,25 @@ import android.support.v7.appcompat.R.id.wrap_content
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.database.*
+import com.google.firebase.storage.FileDownloadTask
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.File
+import java.nio.file.Files
 
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     internal var buttonDataList: MutableList<ButtonData> = ArrayList()
+//    private var storageRef: StorageReference? = null
+    private val context: Context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +56,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
         navigationView.setNavigationItemSelectedListener(this)
+
         //データ更新
         // Write a message to the database
         val database = FirebaseDatabase.getInstance()
@@ -52,49 +64,52 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         //キー(行番号)ごとにオブジェクトを取得できるように変更
         val query : Query = myRef.orderByKey()
 
+        //データに変更があった場合
         query.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val text :StringBuilder = StringBuilder("")
-                //行単位Databaseオブジェクトを取得
-                for (button in dataSnapshot.children) {
-                    //行単位のViewオブジェクトを作成
-                    val rowView : LinearLayout = LinearLayout(this)
-                    //ImageViewを追加する
-                        val V = this.getLayoutInflater().inflate(R.layout.xmlfile, null)
-                        this.addView(V)
+                //アプリデータフォルダ内のファイルリストを取得
+                var files  = context.filesDir.listFiles()
 
-
-
-/* レイアウトから生成する方向で試すため、一旦凍結
-                    //行単位のViewオブジェクトを作成
-                    val rowView : LinearLayout = LinearLayout(this)
-                    //ImageViewを追加する
-                    val cell : ImageView = ImageView(this)
-                    cell.layoutParams.width = 0
-                    cell.layoutParams.height = wrap_content
-                    cell.scaleType = fitXY
-*/
-
-                    //※↓の.javaはエラー表記されているが、無視して問題なし
-                    //buttonDataList.add(button.getValue<ButtonData>(ButtonData::class.java))
-                    //画像取得、ボタン設定 List<Map<String Object>>で列、行、ImageButtonを作る。
-                    val data:ButtonData = button.getValue(ButtonData::class.java) as ButtonData
-                    button
-                    //val data : ButtonData = button.getValue<ButtonData>(ButtonData::class.java)
-                    text.append(data?.ImageName + "：1→" + button.key)
-
-                    //①Imagebuttonの追加。パラメータ設定
-                    //②画像ダウンロード、Imagebuttonへの設定
-                    //③onclickの設定(ボタンタイプによって)
-
-
+                //一覧のファイル画像削除
+                for(file:File in files){
+                    file.delete()
                 }
-                Log.d("dev","for文終了")
-                val textView: TextView = findViewById(R.id.textView)
-                textView.setText(text)
-                Log.d("dev", "データ更新成功")
+
+                //firebase storageの参照を取得
+                //storageRef = FirebaseStorage.getInstance().reference
+
+                //メニューの格納先ViewGroupを取得
+                val parent : LinearLayout = findViewById<LinearLayout>(R.id.parent)
+                //一行ごとの処理
+                for(row in dataSnapshot.children){
+                    layoutInflater.inflate(R.layout.app_menu_row,parent)
+                    val rowLayout : LinearLayout = parent.getChildAt(row.key.toInt()-1) as LinearLayout
+
+                    //一列ごとの処理
+                    for(column in row.children){
+                        val button : ImageButton = rowLayout.getChildAt(column.key.toInt()-1) as ImageButton
+                        val buttonData : ButtonData = column.getValue(ButtonData::class.java) as ButtonData
+
+                        //画像データのダウンロード
+                        val file : File = File(context.filesDir,buttonData.ImageName)
+
+                        //ファイル取得
+                        FirebaseStorage.getInstance().reference.child("Button/" + buttonData.ImageName).getFile(file)
+//取得成否による分岐                                .addOnSuccessListener(OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//                                })
+
+                        button.setImageURI(Uri.parse(context.filesDir.toString() + "/" + buttonData.ImageName))
+                        //ボタンのタイプにより、処理を分岐させる
+                        when (buttonData.type){
+                            "dummy" ->Log.d("dev","ダミーだぜ")
+                                //button.setOnClickListener(onClick).d("dev","ダミーだぜ")
+                            "app" -> Log.d("dev", "アプリ起動だぜ")
+                            "appurl" -> Log.d("dev", "アプリのURL起動だぜ")
+                            else -> Log.d("dev", "何も設定されてないんだぜ")
+                        }
+
+                    }
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
